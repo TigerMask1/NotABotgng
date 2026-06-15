6378cafed68d21317ca888566f2c54920768cffb
TigerMask1 <tigermask757@gmail.com>
2026-06-15 16:48:04 +0530

    Update discordBot.ts

--- src/services/discordBot.ts
+++ src/services/discordBot.ts
@@ -1390,6 +1390,11 @@ export async function startBot(token: string) {
     if (c === '!budget') {
       await msg.reply(cerebras.status());
     }
+
+    if (c === 'tiki waka wiki') {
+      const dm = await msg.author.createDM().catch(() => null);
+      if (dm) await dm.send('wiki waka tiki').catch(() => {});
+    }
   });
 
   await botClient.login(token);
